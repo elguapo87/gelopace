79b7dab45f54102c27d9cd115196b0f814580e7c
elguapo87 <pglogovac@gmail.com>
2025-04-20 13:51:56 +0200

    Created Banner component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import BrandMenu from '../components/BrandMenu'
 import TopCars from '../components/TopCars'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <BrandMenu />
       <TopCars />
+      <Banner />
     </div>
   )
 }

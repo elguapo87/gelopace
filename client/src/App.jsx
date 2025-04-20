eb30c03723c2ca1846ec7b14b8610c38ebd1f184
elguapo87 <pglogovac@gmail.com>
2025-04-20 03:40:59 +0200

    Added components & pages folders & setup react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Cars from "./pages/Cars";
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:brand" element={<Cars />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:carId" element={<Appointment />} />
+      </Routes>
     </div>
   )
 }

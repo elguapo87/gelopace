1703f5d8839dd95e8bb9877b35c595f96379fa90
elguapo87 <pglogovac@gmail.com>
2025-04-21 21:46:28 +0200

    Created usersModel

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,33 @@
-import { createContext } from "react";
-import { cars } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
     const currencySymbol = "€";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [cars, setCars] = useState([]);
+
+    const getCarsData = async () => {
+        try {
+            const { data } = await axios.get(`${backendUrl}/api/cars/car-list`);
+
+            if (data.success) {
+                setCars(data.allCars);
+          
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+            getCarsData();
+        }, []);
 
     const value = {
         cars,

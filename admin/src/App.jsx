f28427cccbf5adfd0a763187a4d19c95f35863a7
elguapo87 <pglogovac@gmail.com>
2025-04-22 15:32:01 +0200

    Added car routes in App.jsx of admin panel & in Sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
+import Login from "./pages/admin/Login";
+import { AdminContext } from "./context/AdminContext";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AdminContext } from "./context/AdminContext";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/admin/Dashboard";
 import AllAppointments from "./pages/admin/AllAppointments";
 import AddCar from "./pages/admin/AddCar";
 import CarList from "./pages/admin/CarList";
-import Login from "./pages/admin/Login";
 import { CarContext } from "./context/CarContext";
-
+import CarAppointments from "./pages/car/CarAppointments";
+import CarDashboard from "./pages/car/CarDashboard";
+import CarProfile from "./pages/car/CarProfile";
 
 const App = () => {
 
@@ -22,22 +24,29 @@ const App = () => {
     <div className="bg-[#f8f9fd]">
       <ToastContainer />
       <Navbar />
-      <div className="flex items-start">         
+      <div className="flex items-start">
         <Sidebar />
         <Routes>
+          {/* Admin Routes */}
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
           <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/add-car" element={<AddCar />} />
           <Route path="/car-list" element={<CarList />} />
+
+          {/* Car Routes */}
+          <Route path="/" element={<></>} />
+          <Route path="/car-appointments" element={<CarAppointments />} />
+          <Route path="/car-dashboard" element={<CarDashboard />} />
+          <Route path="/car-profile" element={<CarProfile />} />
         </Routes>
-      </div>                        
+      </div>
     </div>
 
   ) : (
     <>
-      <Login />
       <ToastContainer />
+      <Login />
     </>
   )
 }
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,36 +1,59 @@
-import { useContext } from 'react';
-import { AdminContext } from "../context/AdminContext";
-import { NavLink } from "react-router-dom";
-import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom"
+import { assets } from "../assets/assets"
+import { useContext } from "react"
+import { AdminContext } from "../context/AdminContext"
+import { CarContext } from "../context/CarContext"
 
 const Sidebar = () => {
 
-  const { aToken } = useContext(AdminContext)
+  const { aToken } = useContext(AdminContext);
+  const { cToken } = useContext(CarContext);
 
   return (
-    <div className='min-h-screen bg-white border-r'>
+    <div className='min-h-screen bg-white border-r max-sm:min-w-[15%]'>
       {
         aToken
           &&
         <ul className='text-[#515151] mt-5'>
           <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/admin-dashboard">
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden sm:block">Dashboard</p>
           </NavLink>
-
+  
           <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/all-appointments">
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden sm:block">Appointments</p>
           </NavLink>
-
+  
           <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/add-car">
             <img src={assets.add_icon} alt="" />
-            <p>Add Car</p>
+            <p className="hidden sm:block">Add Car</p>
           </NavLink>
-
+  
           <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/car-list">
             <img src={assets.people_icon} alt="" />
-            <p>Car List</p>
+            <p className="hidden sm:block">Car List</p>
+          </NavLink>
+        </ul>
+      }
+
+      {
+        cToken
+          &&
+        <ul className='text-[#515151] mt-5'>
+          <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/car-dashboard">
+            <img src={assets.home_icon} alt="" />
+            <p className="hidden sm:block">Dashboard</p>
+          </NavLink>
+  
+          <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/car-appointments">
+            <img src={assets.appointment_icon} alt="" />
+            <p className="hidden sm:block">Appointments</p>
+          </NavLink>
+
+          <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`} to="/car-profile">
+            <img className="w-6" src={assets.car_icon} alt="" />
+            <p className="hidden sm:block">Car Info</p>
           </NavLink>
         </ul>
       }
